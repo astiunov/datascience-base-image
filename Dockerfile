8e688d55ee9e7e4831ee7390587658532d088e93
Alexander Tiunov <astiunov@yandex-team.ru>
2016-11-24 22:14:00 +0300

    Move to /usr

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM ubuntu:xenial
 MAINTAINER Alexander Tiunov <astiunov@yandex-team.ru>
 
 COPY *.yml install.sh ./
-ENV PATH="/home/miniconda3/bin:${PATH}"
+ENV PATH="/usr/miniconda3/bin:${PATH}"
 RUN ./install.sh
--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 locale-gen en_US.UTF-8 && update-locale && apt-get update -y && apt-get install -y bzip2 make build-essential curl
 
-curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >conda_install.sh && chmod +x conda_install.sh && ./conda_install.sh -b -p /home/miniconda3 && rm conda_install.sh
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >conda_install.sh && chmod +x conda_install.sh && ./conda_install.sh -b -p /usr/miniconda3 && rm conda_install.sh
 
-conda install -y jupyter
 conda env create --name py3_env python=3 --file conda_py_env.yml
 
 conda env create --name py2_env python=2 --file conda_py_env.yml
@@ -13,4 +12,4 @@ echo 'install.packages("drat", repos="https://cran.rstudio.com"); drat:::addRepo
 
 rm -rf /tmp/* xgboost.R *.yml *.sh && apt-get clean
 
-chmod -R 0755 /home || true
+chmod -R 0755 /usr/miniconda3 || true
