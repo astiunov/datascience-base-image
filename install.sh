68558dbbe5fb7aab7f73e8462d2a7a371a0b6516
Alexander Tiunov <astiunov@yandex-team.ru>
2016-11-25 22:30:26 +0300

    Fix python2 env

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ locale-gen en_US.UTF-8 && update-locale && apt-get update -y && apt-get install 
 
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >conda_install.sh && chmod +x conda_install.sh && ./conda_install.sh -b -p /usr/miniconda3 && rm conda_install.sh
 
-conda env create --name py3_env python=3 --file conda_py_env.yml
+conda env create --name py3_env --file conda_py3_env.yml
 
-conda env create --name py2_env python=2 --file conda_py_env.yml
+conda env create --name py2_env --file conda_py2_env.yml
 
 conda env create --name r_env --file conda_r_env.yml && source activate r_env
 echo 'install.packages("drat", repos="https://cran.rstudio.com"); drat:::addRepo("dmlc"); install.packages("xgboost", repos="http://dmlc.ml/drat/", type = "source")' >xgboost.R && Rscript xgboost.R
