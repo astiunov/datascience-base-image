3102945bcec50fda26896774b1e7a9603d7edff1
Alexander Tiunov <astiunov@yandex-team.ru>
2016-11-30 18:35:32 +0300

    move r installation to the file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:xenial
 MAINTAINER Alexander Tiunov <astiunov@yandex-team.ru>
 
-COPY *.yml install.sh ./
+COPY *.yml install.sh install_modules.R ./
 ENV PATH="/usr/miniconda3/bin:${PATH}"
 RUN ./install.sh
--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-locale-gen en_US.UTF-8 && update-locale && apt-get update -y && apt-get install -y bzip2 make build-essential curl
+locale-gen en_US.UTF-8 && update-locale && apt-get update -y && apt-get install -y bzip2 make build-essential curl libhdf5-dev
 
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >conda_install.sh && chmod +x conda_install.sh && ./conda_install.sh -b -p /usr/miniconda3 && rm conda_install.sh
+
+conda env create --name r_env --file conda_r_env.yml && source activate r_env
+Rscript install_modules.R
 
 conda env create --name py3_env --file conda_py3_env.yml
 
 conda env create --name py2_env --file conda_py2_env.yml
 
-conda env create --name r_env --file conda_r_env.yml && source activate r_env
-echo 'install.packages("drat", repos="https://cran.rstudio.com"); drat:::addRepo("dmlc"); install.packages("xgboost", repos="http://dmlc.ml/drat/", type = "source")' >xgboost.R && Rscript xgboost.R
-
-rm -rf /tmp/* xgboost.R *.yml *.sh && apt-get clean
+rm -rf /tmp/* install_modules.R *.yml *.sh && apt-get clean
 
 chmod -R 0755 /usr/miniconda3 || true
